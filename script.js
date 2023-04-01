330ff4496f73748d3895f7854d53a9692f889c1c
anandprakash01 <hr.anand0101@gmail.com>
2023-04-01 06:10:36 +0530

    uploading files

--- script.js
+++ script.js
@@ -1,4 +1,95 @@
 
+const slideRow = document.querySelector('.slider-row');
+const prevBtn=document.querySelector(".prev");
+const nextBtn=document.querySelector(".next");
+const interval = 3000;
+
+let slides=document.querySelectorAll(".slide");
+let index = 1;
+let slideID;
+
+const firstClone=slides[0].cloneNode(true);
+const lastClone=slides[slides.length-1].cloneNode(true);
+
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
+
+slideRow.append(firstClone);
+slideRow.prepend(lastClone);
+
+const slideWidth=slides[index].clientWidth;
+// const slideWidth=slides[index].offsetWidth;
+
+slideRow.style.transform=`translateX(${-slideWidth*index}px)`;
+
+
+const startSlide= ()=>{
+    slideID= setInterval(()=>{
+        moveToNextSlide();
+    },interval);
+}
+const getSlides = () => document.querySelectorAll('.slide');
+
+slideRow.addEventListener("transitionend",()=>{
+    slides=getSlides();
+    dotsLabel();
+    if(slides[index].id===firstClone.id){
+        slideRow.style.transition="none";
+        index=1;
+        slideRow.style.transform=`translateX(${-slideWidth*index}px)`;
+    }
+    if(slides[index].id===lastClone.id){
+        slideRow.style.transition="none";
+        index=slides.length-2;
+        slideRow.style.transform=`translateX(${-slideWidth*index}px)`;
+    }
+})
+const moveToNextSlide=()=>{
+    slides=getSlides();
+    if(index>=slides.length-1) return;
+    index++;
+    slideRow.style.transform=`translateX(${-slideWidth*index}px)`;
+    slideRow.style.transition=".7s";
+    dotsLabel("+");
+}
+const moveToPreviousSlide=()=>{
+    slides=getSlides();
+    if(index<=0) return;
+    index--;
+    slideRow.style.transform=`translateX(${-slideWidth*index}px)`;
+    slideRow.style.transition=".7s";
+    dotsLabel("-");
+}
+//on mouse over
+slideRow.addEventListener("mouseenter",()=>{
+    clearInterval(slideID);
+});
+slideRow.addEventListener("mouseleave",startSlide);
+nextBtn.addEventListener('click', moveToNextSlide);
+prevBtn.addEventListener("click", moveToPreviousSlide);
+startSlide();
+
+
+//dots
+let dots=document.getElementsByClassName("dot");
+function dotsLabel(sign){
+    if(index>dots.length){
+        dots[dots.length-1].className=dots[dots.length-1].className.replace("active","");
+        dots[0].classList.add("active");
+        return;
+    }
+    if(index<=0){
+        dots[0].className=dots[0].className.replace("active","");
+        dots[dots.length-1].classList.add("active");
+        return;
+    }
+    for(let i=0;i<dots.length;i++){
+        dots[i].className=dots[i].className.replace("active","");
+    }
+    
+    dots[index-1].classList.add("active");
+}
+
 
 
 
@@ -38,4 +129,4 @@ brandRow.addEventListener('transitionend', ()=>{
     
 });
 
-setInterval(nextBrand,1000);
+setInterval(nextBrand,3000);
